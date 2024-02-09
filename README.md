7d21c3e93e4f4d1fa73f927fa1a4179650b50c8a
unknown <companydams@gmail.com>
2024-02-09 10:12:46 -0500

    Creación Tienda Virtual CALEB

---
+++ README.md
@@ -0,0 +1,64 @@
+# Vue 3 Ecommerce
+
+## Entities
+
+## Product
+
+- id
+- name
+- description
+- price
+- Image
+
+### Category
+
+- name
+- description
+
+## Cart
+
+- products: `[{productId: 1, quantity: 3},{productId: 7, quantity: 5}]`
+
+## Components
+
+### ProductCard
+
+## Pages
+
+/ -> Todos los productos
+/category/5 -> Productos sólo de la categoría 5
+/Cart -> Ver el carrito de compras
+
+
+
+
+
+
+
+
+
+
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
---
+++ src/components/TopBar.vue
@@ -0,0 +1,61 @@
+<script lang="ts">
+import { useCartStore } from '@/stores/cart';
+
+export default {
+    computed: {
+        itemsCount() {
+            const cartStore = useCartStore();
+            return cartStore.cartItemsCount;
+        }
+    }
+}
+</script>
+
+<template>
+    <v-app-bar flat>
+      <v-container class="mx-auto d-flex align-center justify-center">
+        <v-avatar
+          class="me-10 ms-4 "
+          size="32">
+          <v-img src="/logodams.png" />
+          </v-avatar>
+
+        <RouterLink to="/" custom="" v-slot="{navigate}">
+          <v-btn :active="$route.name === 'home' || $route.name === 'category'" variant="text" @click="navigate">
+            <v-icon class="mr-0 mr-sm-2" icon="mdi-home-account" />
+            <span class="d-none d-sm-flex">Home</span>  
+          </v-btn>
+        </RouterLink>
+        
+        <RouterLink to="/cart" custom="" v-slot="{navigate}">
+          <v-badge :content="itemsCount" :model-value="itemsCount > 0" color="orange-lighten-2">
+            <v-btn :active="$route.name === 'cart'" variant="text" @click="navigate">
+              <v-icon class="mr-0 mr-sm-2" icon="mdi-cart-heart" />
+              <span class="d-none d-sm-flex">Cart</span>
+            </v-btn>
+          </v-badge>          
+        </RouterLink>
+        
+        <RouterLink to="/about" custom="" v-slot="{navigate}">
+          <v-btn :active="$route.name === 'about'" variant="text" @click="navigate">
+            <v-icon class="mr-0 mr-sm-2" icon="mdi-information" />
+            <span class="d-none d-sm-flex">About</span>
+          </v-btn>
+        </RouterLink>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="160">
+          <v-text-field
+            density="compact"
+            flat
+            hide-details
+            label="Search"
+            rounded="lg"
+            single-line
+            variant="solo-filled"
+          ></v-text-field> 
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+</template>
